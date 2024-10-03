646543f1c3c9c869ce7aaac7a713e18be66394ea
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-03 22:45:48 +0600

    finding element at js file

---
+++ index.js
@@ -0,0 +1,7 @@
+// finding elements
+const form = document.querySelector("form");
+const cardBody = document.querySelector(".card-body");
+const guessingNumber = document.querySelector("#guessingNumber");
+const checkButton = form.querySelector("#check");
+const resultText = cardBody.querySelector(".esultText");
+const remainingAttempts = cardBody.querySelector(".remainingAttempts");
